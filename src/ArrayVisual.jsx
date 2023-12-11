475e5a66f506c60b4ebeb9d7a8566576e30cf814
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2023-12-10 22:46:25 -0500

    array

--- src/ArrayVisual.jsx
+++ src/ArrayVisual.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 
 function ArrayVisual() {
     const [numElements, setNumElements] = useState(12);
+    const [initial, setInitial] = useState();
     let elements = []
     for(let i = 0; i < numElements; i++) {
         elements.push(
             <>
             <div className="element" key={i}>
-                0
+                {initial}
             </div>
             </>
         )
@@ -16,11 +17,26 @@ function ArrayVisual() {
 
     return (
         <>
-        <input type="text" placeholder={"number of elements"} id = "input" onChange={
-            () => {
-                setNumElements(parseInt(document.getElementById("input").value))
-            }
-        }/>
+        <p style={ {fontWeight : "bold"} }>
+            int array[
+            <input type="text" size={1} maxLength={2} id = "size" onChange={
+                () => {
+                    setNumElements(parseInt(document.getElementById("size").value))
+                }
+            }/>
+            ] = &#123; 
+            <input type="text" size={1} maxLength={2} id = "initial" onChange={
+                () => {
+                    if (isNaN (parseInt(document.getElementById("initial").value))) {
+                        setInitial()
+                    } else {
+                        setInitial(parseInt(document.getElementById("initial").value))
+                    }
+                    
+                }
+            }/>
+            &#125;
+        </p>
         
         <div className="ArrayVisual" style={ {"gridTemplateColumns" : `repeat(${numElements}, [element] 1fr)`, "aspectRatio" : `${numElements}`} }>
             {elements}
