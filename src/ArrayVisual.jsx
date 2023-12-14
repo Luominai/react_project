a7b5e47633f402b99eb97364f868ad7cc8e966dd
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2023-12-14 14:42:59 -0500

    option 2

--- src/ArrayVisual.jsx
+++ src/ArrayVisual.jsx
@@ -1,76 +1,92 @@
 import "./ArrayVisual.css"
 import { useState } from "react";
 
+
 function ArrayVisual() {
-    const [numElements, setNumElements] = useState(12);
-    const [initial, setInitial] = useState();
+    const [array, setArray] = useState(["", "", "", "", "", "", "", "", "", "", "", ""])
+    // const [numElements, setNumElements] = useState(12);
     let elements = []
-    for(let i = 0; i < numElements; i++) {
+    for(let i = 0; i < array.length; i++) {
         elements.push(
             <>
             <div className="element" key={i}>
-                {initial}
+                {array[i]}
             </div>
             </>
         )
     }
 
+    function ClearExcept(excludedID) {
+        let buttons = document.querySelectorAll('input[type="radio"]:checked')
+        for(let i = 0; i < buttons.length; i++) {
+            if (buttons[i].id != excludedID) {
+                buttons[i].checked = false;
+            }
+        }
+    }
+
     function InitWithDefault() {
-        return (
-            <>
-            <span style={ {fontWeight : "bold"} }>
-            int array[
-            <input type="text" size={1} maxLength={2} id = "size" onChange={
-                () => {
-                    setNumElements(parseInt(document.getElementById("size").value))
-                }
-            }/>
-            ] = &#123; 
-            <input type="text" size={1} maxLength={2} id = "initial" onChange={
-                () => {
-                    if (isNaN (parseInt(document.getElementById("initial").value))) {
-                        setInitial()
-                    } else {
-                        setInitial(parseInt(document.getElementById("initial").value))
-                    }
-                    
-                }
-            }/>
-            &#125;
-        </span></>
-        )
+        if (document.getElementById("InitWithDefault").checked) {
+            // get array size and default value
+            let size = parseInt(document.getElementById("size").value)
+            let initial = parseInt(document.getElementById("initial").value)
+
+            let newArray = [];
+            for(let i = 0; i < size; i++) {
+                newArray.push(initial)
+            }
+
+            setArray(newArray) 
+        }
     }
 
     function InitExplicit() {
-        return (
-            <>
-            <span style={ {fontWeight : "bold"} }>
-            int array[] = &#123; 
-            <input type="text" size={3} id = "explicit" onChange={
-                () => {
-                    if (isNaN (parseInt(document.getElementById("explicit").value))) {
-                        setInitial()
-                    } else {
-                        setInitial(parseInt(document.getElementById("explicit").value))
-                    }
-                    
-                }
-            }/>
-            &#125;
-        </span></>
-        )
+        if (document.getElementById("InitExplicit").checked) {
+            // parse list
+            let input = document.getElementById("explicit").value.split(",")
+            let newArray = []
+            for(let i = 0; i < input.length; i++) {
+                newArray.push(parseInt(input[i]))
+            }   
+            setArray(newArray)
+        }
     }
 
     return (
         <>
         <form>
             <label>
-                <input type="radio" id="InitWithDefault"></input>
-                {InitWithDefault()}
+                <input type="radio" id="InitWithDefault" onClick={
+                    () => {
+                        ClearExcept("InitWithDefault")
+                        InitWithDefault()
+                    }
+                }></input>
+                <span style={ {fontWeight : "bold"} }>
+                    int array[
+                    <input type="text" size={1} maxLength={2} id = "size" onChange={InitWithDefault}/>
+                    ] = &#123; 
+                    <input type="text" size={1} maxLength={2} id = "initial" onChange={InitWithDefault}/>
+                    &#125;
+                </span>
+
+                <br></br>
+
+                <input type="radio" id="InitExplicit" onClick={
+                    () => {
+                        ClearExcept("InitExplicit")
+                        InitExplicit()
+                    }
+                }></input>
+                <span style={ {fontWeight : "bold"} }>
+                    int array[] = &#123; 
+                    <input type="text" size={15} id = "explicit" onChange={InitExplicit}/>
+                    &#125;
+                </span>
             </label> 
         </form>
         
-        <div className="ArrayVisual" style={ {"gridTemplateColumns" : `repeat(${numElements}, [element] 1fr)`, "aspectRatio" : `${numElements}`} }>
+        <div className="ArrayVisual" style={ {"gridTemplateColumns" : `repeat(${array.length}, [element] 1fr)`, "aspectRatio" : `${array.length}`} }>
             {elements}
         </div>
         </>
